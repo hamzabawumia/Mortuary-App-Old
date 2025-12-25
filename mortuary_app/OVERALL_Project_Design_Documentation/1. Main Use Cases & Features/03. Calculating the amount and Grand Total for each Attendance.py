6de644b7c683d5722724dad1b02a883becadc502
Dr Hamza Bawumia <hamzabawumia@yahoo.com>
2025-12-25 08:10:41 +0000

    Update 03. Calculating the amount and Grand Total for each Attendance.py

--- mortuary_app/OVERALL_Project_Design_Documentation/1. Main Use Cases & Features/03. Calculating the amount and Grand Total for each Attendance.py
+++ mortuary_app/OVERALL_Project_Design_Documentation/1. Main Use Cases & Features/03. Calculating the amount and Grand Total for each Attendance.py
@@ -21,3 +21,18 @@ What it means is that we need to tell the users not to edit any paid invoice
 or we need to make any paid invoice uneditable after payment.
 If there are any new changes then a new invoice should be created.
 """
+
+how order-line pricing & encounter totals are calculated/displayed.
+--------------------------------------------------------------------
+
+Your actual architecture is:
+
+Parent Doctype.JS owns behavior
+
+Child JS only reacts (no js code is in the childtable.JS)
+
+Pricing is queried and inserted via client-side
+
+Totals are derived , also client side
+
+
